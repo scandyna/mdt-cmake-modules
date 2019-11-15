c2b90b603cc2f1fce82ddcc800a703d3deec779e
Philippe Steinmann <philippe.steinmann@gmail.com>
2019-11-15 10:41:45 +0100

    Work on installing includes

--- Modules/MdtInstallLibrary.cmake
+++ Modules/MdtInstallLibrary.cmake
@@ -5,8 +5,275 @@
 # MdtInstallLibrary
 # -----------------
 #
-# Install include directories
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# Headers in various project layouts
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Some examples of source tree.
+# Some are inspired from `The Pitchfork Layout (PFL) <https://github.com/vector-of-bool/pitchfork>`_
+#
+# Separate Header Placement::
+#
+#   ${CMAKE_SOURCE_DIR}
+#     |-CMakeLists.txt
+#     |-include
+#     |   |-Mdt
+#     |      |-LedPainter.h
+#     |      |-LedWidget.h
+#     |-src
+#        |-CMakeLists.txt
+#        |-Mdt
+#           |-LedPainter.cpp
+#           |-LedWidget.cpp
+#
+#
+# For above example, in the ``CMakeLists.txt`` in ``src``, the include directory could be declared:
+#
+# .. code-block:: cmake
+#
+#   target_include_directories(Mdt_Led
+#     PUBLIC
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/../include>
+#   )
+#
+# Merged Header Placement::
+#
+#   ${CMAKE_SOURCE_DIR}
+#     |-CMakeLists.txt
+#     |-src
+#        |-CMakeLists.txt
+#        |-Mdt
+#           |-LedPainter.h
+#           |-LedWidget.h
+#           |-LedPainter.cpp
+#           |-LedWidget.cpp
+#
+#
+# For above example, in the ``CMakeLists.txt`` in ``src``, the include directory could be declared:
+#
+# .. code-block:: cmake
+#
+#   target_include_directories(Mdt_Led
+#     PUBLIC
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+#   )
+#
+# Project using Submodules (with Merged Header Placement) ::
+#
+#   ${CMAKE_SOURCE_DIR}
+#     |-CMakeLists.txt
+#     |-libs
+#        |-ItemModel
+#            |-src
+#               |-CMakeLists.txt
+#               |-Mdt
+#                  |-ItemModel
+#                     |-SortProxyModel.h
+#                     |-SortProxyModel.cpp
+#
+# For above example, in the ``CMakeLists.txt`` in ``src``, the include directory could be declared:
+#
+# .. code-block:: cmake
+#
+#   target_include_directories(Mdt_ItemModel
+#     PUBLIC
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+#   )
+#
+# Some headers are generated during build time.
+# Such an example are export headers, generated using :command:`generate_export_header()`.
+# Taking the above example "Merged Header Placement", the build directory tree could be similar to::
+#
+#   ${CMAKE_BINARY_DIR}
+#       |-CMakeFiles
+#       |-CMakeCache.txt
+#       |-OtherCmakeFilesAndDirectories
+#       |-src
+#          |-CMakeFiles
+#          |-Mdt_Led_autogen
+#          |   |-include
+#          |   |-moc_predefs.h
+#          |-libMdt_Led.so
+#          |-Makefile
+#          |-MdtLedExport.h
+#
+# We can see that a lot of files and directories are generated in the build tree.
+# In above example, only ``MdtLedExport.h`` has to be installed, but not ``moc_predefs.h``.
+#
+# Install headers with CMake
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Lets take the above example "Merged Header Placement" again to explore various
+# possible ways to define and install headers.
+#
+# Example using :command:`install(DIRECTORY)`:
+#
+# .. code-block:: cmake
+#
+#   add_library(Mdt_Led
+#     Mdt/LedPainter.cpp
+#     Mdt/LedWidget.cpp
+#   )
+#
+#   # Define the include directory we need to buil and those needed by the user of this target
+#   # Do not define the INSTALL_INTERFACE here, see the install(TARGETS) below
+#   target_include_directories(Mdt_Led
+#     PUBLIC
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
+#   )
+#
+#   set(MDT_INSTALL_PACKAGE_NAME Mdt0)
+#   include(GNUInstallDirs)
+#   include(MdtInstallDirs)
+#
+#   # Install the binary library of this target
+#   # The INCLUDES DESTINATION specifies a list of directories
+#   # which will be added to the INTERFACE_INCLUDE_DIRECTORIES target property of the <targets>
+#   # when exported by the install(EXPORT) command
+#   # But, the includes directories are not installed
+#   install(
+#     TARGETS Mdt_Led
+#     EXPORT Mdt_LedTargets
+#     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+#     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+#     ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+#     INCLUDES DESTINATION ${MDT_INSTALL_INCLUDEDIR}
+#   )
+#
+#   install(
+#     EXPORT Mdt_LedTargets
+#     DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Mdt0Led
+#     NAMESPACE Mdt0::
+#     FILE Mdt0LedTargets.cmake
+#   )
+#
+#   # Install the includes directory
+#   install(
+#     DIRECTORY Mdt
+#     DESTINATION ${MDT_INSTALL_INCLUDEDIR}
+#     FILES_MATCHING PATTERN *.h
+#   )
+#
+# Once the above example is installed, it looks like::
+#
+#   ${CMAKE_INSTALL_PREFIX}
+#       |-include
+#       |   |-Mdt
+#       |      |-LedPainter.h
+#       |      |-LedWidget.h
+#       |-lib
+#          |-cmake
+#          |   |-Mdt0Led
+#          |       |-Mdt0LedTargets.cmake
+#          |-libMdt_Led.so
+#
+# The genarated ``Mdt0LedTargets.cmake`` file will define the ``INTERFACE_INCLUDE_DIRECTORIES`` property to the IMPORTED target:
+#
+# .. code-block:: cmake
+#
+#   add_library(Mdt0::Led SHARED IMPORTED)
+#
+#   set_target_properties(Mdt0::Led PROPERTIES
+#     INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+#   )
+#
+# If we do not specify the ``INCLUDES DESTINATION`` argument of :command:`install(TARGETS)`,
+# the ``INTERFACE_INCLUDE_DIRECTORIES`` property will not be set to the IMPORTED target.
+#
+# CMake also provides the :variable:`PUBLIC_HEADER` and :variable:`PRIVATE_HEADER` target properties:
+#
+# .. code-block:: cmake
+#
+#   add_library(Mdt_Led
+#     Mdt/LedPainter.cpp
+#     Mdt/LedWidget.cpp
+#   )
+#
+#   # We still have to define the includes directories to build this target,
+#   # but also to for the user of this target in our project, for example unit tests
+#   # Do not define the INSTALL_INTERFACE here, see the install(TARGETS) below
+#   target_include_directories(Mdt_Led
+#     PUBLIC
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
+#   )
+#
+#   # Set the PUBLIC_HEADER property to this target
+#   # Notice how multiple files have to be specified
+#   # If we not provide the list in a quoted string,
+#   # only the first header will be set to the property
+#   set_target_properties(Mdt_Led
+#     PROPERTIES
+#       PUBLIC_HEADER "Mdt/LedPainter.h;Mdt/LedWidget.h"
+#   )
+#
+#   set(MDT_INSTALL_PACKAGE_NAME Mdt0)
+#   include(GNUInstallDirs)
+#   include(MdtInstallDirs)
+#
+#   # Install the binary library of this target
+#   # This time, the headers existing in the PUBLIC_HEADER property of this target
+#   # will be installed to the location specified by the PUBLIC_HEADER DESTINATION argument
+#   # We still have to provide the INCLUDES DESTINATION,
+#   # else the INTERFACE_INCLUDE_DIRECTORIES property will not be set to the imported target
+#   install(
+#     TARGETS Mdt_Led
+#     EXPORT Mdt_LedTargets
+#     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+#     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+#     ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+#     PUBLIC_HEADER DESTINATION ${MDT_INSTALL_INCLUDEDIR}/Mdt
+#     INCLUDES DESTINATION ${MDT_INSTALL_INCLUDEDIR}
+#   )
+#
+#   install(
+#     EXPORT Mdt_LedTargets
+#     DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/Mdt0Led
+#     NAMESPACE Mdt0::
+#     FILE Mdt0LedTargets.cmake
+#   )
+#
+# Notice that ``PUBLIC_HEADER DESTINATION`` points to ``Mdt`` subdirectory.
+# Here the directory structure is not copied, but each file specified
+# in the ``PUBLIC_HEADER`` property is copied to ``PUBLIC_HEADER DESTINATION``.
+# The ``INCLUDES DESTINATION`` has also to be specified,
+# so that the :variable:`INTERFACE_INCLUDE_DIRECTORIES` is defined in the IMPORTED target.
+# Notice also that we not specify the ``Mdt`` subdirectory to ``INCLUDES DESTINATION``.
+#
+# Helper functions to install hedares
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# .. command:: mdt_install_include_directory
+#
+# Install a include directory::
+#
+#   mdt_install_include_directory(
+#     DIRECTORY <dir>
+#     DESTINATION <dir>
+#     [FILE_EXTENSIONS ext1 [ext2 ...]]
+#     [FILE_WITHOUT_EXTENSION]
+#     [COMPONENT <component>]
+#   )
+#
+# The headers in ``DIRECTORY`` will be installed.
+#
+# By default, files with extensions ``.h``, ``.hpp``, ``.hh`` will be installed.
+# A alternate list of file extensions can be passed as ``FILE_EXTENSIONS``.
+# If the ``FILE_WITHOUT_EXTENSION`` option is set, files without extensions will also be installed.
+#
+#
+# .. command:: mdt_install_include_files
+#
+# Install include files::
+#
+#   mdt_install_include_files(
+#     DIRECTORY <dir>
+#     DESTINATION <dir>
+#     [FILE_EXTENSIONS ext1 [ext2 ...]]
+#     [FILE_WITHOUT_EXTENSION]
+#     [COMPONENT <component>]
+#   )
+#
 #
 # .. command:: mdt_install_interface_include_directories
 #
@@ -15,18 +282,49 @@
 #   mdt_install_interface_include_directories(
 #     TARGET <target>
 #     DESTINATION <dir>
-#     [FILES_MATCHING <pattern|regex>]
+#     [FILE_EXTENSIONS ext1 [ext2 ...]]
+#     [FILE_WITHOUT_EXTENSION]
+#     BINARY_DIR_INCLUDES_DESTINATION <dir>
+#     [BINARY_DIR_INCLUDES_FILE_EXTENSIONS ext1 [ext2 ...]]
+#     [COMPONENT <component>]
 #   )
 #
 # The headers of all directories specified in the ``INTERFACE_INCLUDE_DIRECTORIES`` property of ``target`` will be installed.
-# By default, à regex matching ``*.h``, ``*.hpp``, ``*.hh`` and files with no extension is used to filter which files must be copied.
-# An alternate filter can be passed as ``FILES_MATCHING`` argument,
-# which will be passed to the ``FILES_MATCHING`` argument of :command:`install(DIRECTORY)`.
+#
+# See also :command:`mdt_install_include_directory()`
+#
+# TODO: will not work for headers in BUILD directories:
+# - Will copy unwanted directory structure (all subdirectories generated by CMake, f.ex)
+# - If FILE_WITHOUT_EXTENSION is set, will also copy files like Makefile
+#
+# Example of something (current!) that will fail:
+#
+# .. code-block:: cmake
+#
+#   target_include_directories(Mdt_ItemEditor
+#     PUBLIC
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>
+#       $<INSTALL_INTERFACE:include>
+#     PRIVATE
+#       $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/Impl>
+#   )
+#
+# NOTE: use mdt_install_include_directory() for all that is not in ${CMAKE_BINARY_DIR}.
+# Also give optional directories to ignore ?
+# For that is in BUILD tree, use mdt_install_include_files() .
+#
+# mdt_install_include_directory() will copy the source directory structures (subdirectories),
+# mdt_install_include_files() will copy given files to destination dir.
+#
+#
+# TODO: seems difficult to solve properly. Should:
+#  - Specify explicitly which directories containing includes to install in mdt_install_library()
+#  - Specify explicitly other files to install (f.ex. exports)
+#  - Maybe target property ``INSTALL_INCLUDE_DIRECTORIES`` + ``INSTALL_ADDITIONAL_INCLUDE_FILES`` + ``INSTALL_ADDITIONAL_INCLUDE_FILES_DESTINATION`` (could be set by mdt_add_library()).
 #
 # Install a library
 # ^^^^^^^^^^^^^^^^^
-#
-# TODO Public and Private dependencies
 #
 # .. command:: mdt_install_library
 #
@@ -37,8 +335,13 @@
 #     RUNTIME_DESTINATION <dir>
 #     LIBRARY_DESTINATION <dir>
 #     ARCHIVE_DESTINATION <dir>
-#     INCLUDES_DESTINATION <dir>
-#     [INCLUDES_FILES_MATCHING <pattern|regex>]
+#     [INCLUDES_DIRECTORY <dir>]
+#     [INCLUDES_FILE_EXTENSIONS ext1 [ext2 ...]]
+#     [INCLUDES_FILE_WITHOUT_EXTENSION]
+#     [ADDITIONAL_INCLUDES_FILES file1 [file2 ...]]
+#     [INCLUDES_DESTINATION <dir>]
+#     [BINARY_DIR_INCLUDES_DESTINATION <dir>]
+#     [BINARY_DIR_INCLUDES_FILE_EXTENSIONS ext1 [ext2 ...]]
 #     EXPORT_NAME <export-name>
 #     EXPORT_NAMESPACE <export-namespace>
 #     INSTALL_NAMESPACE <install-namespace>
@@ -53,10 +356,18 @@
 #
 # Install ``target`` using :command:`install(TARGETS)` to the various given destinations.
 #
-# Will also install the headers to the destination specified by ``INCLUDES_DESTINATION`` using :command:`mdt_install_interface_include_directories()`.
-# A default filter is used to match common header files.
-# A alternate filter can be passed as ``INCLUDES_FILES_MATCHING``,
-# which will be passed to the ``FILES_MATCHING`` argument of :command:`install(DIRECTORY)`.
+# Will also install the includes directory passed as ``INCLUDES_DIRECTORY`` argument
+# to the destination specified by ``INCLUDES_DESTINATION`` using :command:`mdt_install_interface_include_directories()`.
+# For more informations about ``INCLUDES_FILE_EXTENSIONS`` and ``INCLUDES_FILE_WITHOUT_EXTENSION``
+# see :command:`mdt_install_include_directory()`.
+#
+# Header files passed to ``ADDITIONAL_INCLUDES_FILES`` will also be installed,
+# using :command:`install(FILES)`,
+# to the location defined by the ``INCLUDES_DESTINATION`` argument.
+#
+# The ``INCLUDES_DESTINATION`` will also be passed to :command:`install(TARGETS)`
+# as ``INCLUDES DESTINATION`` argument.
+# This way, the ``INTERFACE_INCLUDE_DIRECTORIES`` will be set for the IMPORTED target.
 #
 # The ``EXPORT_NAME`` and ``EXPORT_NAMESPACE`` will be set as properties to ``target``:
 #
@@ -109,7 +420,9 @@
 # .. code-block:: cmake
 #
 #   add_library(Mdt_ItemEditor
-#     sourc1.cpp sourc2.cpp ...
+#     Mdt/ItemEditor/sourc1.cpp
+#     Mdt/ItemEditor/sourc2.cpp
+#     ...
 #   )
 #
 #   target_link_libraries(Mdt_ItemEditor
@@ -134,6 +447,9 @@
 #     RUNTIME_DESTINATION ${CMAKE_INSTALL_BINDIR}
 #     LIBRARY_DESTINATION ${CMAKE_INSTALL_LIBDIR}
 #     ARCHIVE_DESTINATION ${CMAKE_INSTALL_LIBDIR}
+#     INCLUDES_DIRECTORY .
+#     INCLUDES_FILE_WITHOUT_EXTENSION
+#     ADDITIONAL_INCLUDES_FILES "${CMAKE_CURRENT_BINARY_DIR}/MdtLedExport.h"
 #     INCLUDES_DESTINATION ${MDT_INSTALL_INCLUDEDIR}
 #     EXPORT_NAME ItemEditor
 #     EXPORT_NAMESPACE Mdt0::
@@ -169,6 +485,36 @@
 #           |-Mdt0ItemEditorConfig.cmake
 #           |-Mdt0ItemEditorConfigVersion.cmake
 #
+# The generated ``Mdt0ItemEditorTargets.cmake`` will define the IMPORTED target:
+#
+# .. code-block:: cmake
+#
+#   add_library(Mdt0::ItemEditor SHARED IMPORTED)
+#
+#   set_target_properties(Mdt0::ItemEditor PROPERTIES
+#     INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
+#     INTERFACE_LINK_LIBRARIES "Mdt0::ItemModel"
+#   )
+#
+# The generated ``Mdt0ItemEditorConfig.cmake`` will look like:
+#
+# .. code-block:: cmake
+#
+#   # Find dependencies for target Mdt0::ItemEditor
+#   find_package(Mdt0ItemModel 0.1.2 QUIET CONFIG PATHS "${CMAKE_CURRENT_LIST_DIR}/.." NO_DEFAULT_PATH)
+#   if(NOT Mdt0ItemModel_FOUND)
+#     find_package(Mdt0ItemModel 0.1.2 QUIET REQUIRED CONFIG)
+#   endif()
+#
+#   include("${CMAKE_CURRENT_LIST_DIR}/Mdt0ItemEditorTargets.cmake")
+#   # Set package properties for target Mdt0::ItemEditor
+#   set_target_properties(Mdt0::ItemEditor
+#     PROPERTIES
+#       INTERFACE_FIND_PACKAGE_NAME Mdt0ItemEditor
+#       INTERFACE_FIND_PACKAGE_VERSION 0.1.2
+#       INTERFACE_FIND_PACKAGE_PATHS ..
+#   )
+#
 #
 # Example of a system wide install on a Debian MultiArch (``CMAKE_INSTALL_PREFIX=/usr``)::
 #
@@ -193,6 +539,8 @@
 #
 #
 #
+#
+#
 # Once the library is installed, the user should be able to find it using CMake ``find_package()`` in its ``CMakeLists.txt``:
 #
 # .. code-block:: cmake
@@ -214,6 +562,62 @@
 
 include(MdtTargetProperties)
 include(MdtPackageConfigHelpers)
+
+
+function(mdt_install_include_directory)
+
+  set(options FILE_WITHOUT_EXTENSION)
+  set(oneValueArgs DIRECTORY DESTINATION)
+  set(multiValueArgs FILE_EXTENSIONS)
+  cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  if(NOT ARG_DIRECTORY)
+    message(FATAL_ERROR "mdt_install_include_directory(): mandatory argument DIRECTORY missing")
+  endif()
+  if(NOT ARG_DESTINATION)
+    message(FATAL_ERROR "mdt_install_include_directory(): mandatory argument DESTINATION missing")
+  endif()
+
+  set(fileExtensions)
+  if(ARG_FILE_EXTENSIONS)
+    set(fileExtensions ${ARG_FILE_EXTENSIONS})
+  else()
+    set(fileExtensions ".h" ".hpp" ".hh")
+  endif()
+
+  list(GET fileExtensions 0 firstExtension)
+  list(REMOVE_AT fileExtensions 0)
+  set(regex "/.+\\${firstExtension}")
+  foreach(extension ${fileExtensions})
+    string(APPEND regex "|/.+\\${extension}")
+  endforeach()
+
+  if(ARG_FILE_WITHOUT_EXTENSION)
+    string(APPEND regex "|/[^.]+$")
+  endif()
+
+  message("REGEX: ${regex}")
+
+  set(componentArguments)
+  if(ARG_COMPONENT)
+    set(componentArguments COMPONENT ${ARG_COMPONENT})
+  endif()
+
+  install(
+    DIRECTORY "${ARG_DIRECTORY}"
+    DESTINATION "${ARG_DESTINATION}"
+    ${componentArguments}
+    FILES_MATCHING
+      REGEX "${regex}"
+  )
+
+endfunction()
+
+
+function(mdt_install_include_directory)
+
+endfunction()
+
 
 function(mdt_install_library)
 
