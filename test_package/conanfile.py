3d862ca2b9c52790d0f00297d5dfc487b77722b5
Philippe Steinmann <philippe.steinmann@gmail.com>
2019-11-17 12:23:46 +0100

    Work on tests

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,11 +10,13 @@ class MdtCMakeModulesTestConan(ConanFile):
   def build(self):
     cmake = CMake(self)
     cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "%s/conan_paths.cmake" % (self.build_folder)
-    cmake.configure()
+    cmake.definitions["BUILD_TESTS"] = "ON"
+    cmake.configure(source_folder="..")
 
   def imports(self):
     self.copy("*.dll", dst="bin", src="bin")
 
   def test(self):
     cmake = CMake(self)
+    cmake.test()
     # We have no test to run, here we fake a bit..
