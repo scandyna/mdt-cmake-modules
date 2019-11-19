0ad900ea1153790f7e7f87f3af99d953003d2695
Philippe Steinmann <philippe.steinmann@gmail.com>
2019-11-19 06:18:36 +0100

    Work on tests

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,8 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
 
-# TODO shared libraries !!
-
 class MdtCMakeModulesTestConan(ConanFile):
   settings = "os"
   generators = "cmake_paths"
@@ -10,13 +8,11 @@ class MdtCMakeModulesTestConan(ConanFile):
   def build(self):
     cmake = CMake(self)
     cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "%s/conan_paths.cmake" % (self.build_folder)
-    cmake.definitions["BUILD_TESTS"] = "ON"
-    cmake.configure(source_folder="..")
+    cmake.configure()
 
   def imports(self):
     self.copy("*.dll", dst="bin", src="bin")
 
   def test(self):
     cmake = CMake(self)
-    cmake.test()
     # We have no test to run, here we fake a bit..
--- tests/libs/ItemEditor/src/Mdt/ItemEditor/SortSetupWidget.h
+++ tests/libs/ItemEditor/src/Mdt/ItemEditor/SortSetupWidget.h
@@ -1,1 +1,2 @@
- 
+
+#include <Mdt/ItemModel/SortProxyModel>
