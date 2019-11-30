0753a7151278adfae202354c0468cf484fbcf033
Philippe Steinmann <philippe.steinmann@gmail.com>
2019-11-30 12:25:21 +0100

    Work on tests

--- tests/libs/ItemModel/conanfile.py
+++ tests/libs/ItemModel/conanfile.py
@@ -12,19 +12,14 @@ class MdtCmakeModulesTestMdtItemModelConan(ConanFile):
   default_options = {"shared": True}
   requires = "MdtCMakeModules/[>0.1]@MdtCMakeModules_tests/testing"
   generators = "cmake_paths"
-  exports_sources="*" # Conan seems to be smart enough to not copy test_package/build
+  exports_sources="src/*", "CMakeLists.txt"
 
   def build(self):
     cmake = CMake(self)
     cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "%s/conan_paths.cmake" % (self.build_folder)
-    cmake.configure()
+    cmake.configure(cache_build_folder = "build")
     cmake.build()
-
-  def package(self):
-    cmake = CMake(self)
     cmake.install()
 
   def package_info(self):
     self.env_info.LD_LIBRARY_PATH.append(os.path.join(self.package_folder, "lib"))
-
-# TODO See Conan doc about instantiation (build() / package() ) When build() and package() are run in isolation, we could have problems !
