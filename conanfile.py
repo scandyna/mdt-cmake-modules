f27a6514ccdfbfb7efeffefc50ba139e9de01b62
Philippe Steinmann <philippe.steinmann@gmail.com>
2019-12-08 09:00:29 +0100

    experiment common version

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,16 @@ from conans import ConanFile, CMake, tools
 
 class MdtCMakeModulesConan(ConanFile):
   name = "MdtCMakeModules"
-  version = "0.6"
+  #version = "0.6"
   license = "BSD 3-Clause"
   url = "https://github.com/scandyna/mdt-cmake-modules"
   description = "Some CMake modules used in \"Multi Dev Tools\" projects"
   generators = "cmake_paths"
   exports_sources = "Modules/*", "CMakeLists.txt", "MdtCMakeModulesConfig.cmake.in", "LICENSE"
+
+  def set_version(self):
+    git = tools.Git()
+    self.version = "%s" % (git.get_tag())
 
   def build(self):
     cmake = CMake(self)
