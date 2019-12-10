73e600f897bea3b3dfab4c6beaab53e9a5f71b48
Philippe Steinmann <philippe.steinmann@gmail.com>
2019-12-10 05:45:41 +0100

    Update URL to GitLab

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class MdtCMakeModulesConan(ConanFile):
   name = "MdtCMakeModules"
   #version = "0.6"
   license = "BSD 3-Clause"
-  url = "https://github.com/scandyna/mdt-cmake-modules"
+  url = "https://gitlab.com/scandyna/mdt-cmake-modules"
   description = "Some CMake modules used in \"Multi Dev Tools\" projects"
   generators = "cmake_paths"
   exports_sources = "Modules/*", "CMakeLists.txt", "MdtCMakeModulesConfig.cmake.in", "LICENSE"
