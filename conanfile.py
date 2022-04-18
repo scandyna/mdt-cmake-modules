a42cb0a93398803458e31fb24ba599d56376b4ea
Philippe Steinmann <philippe.steinmann@gmail.com>
2022-04-18 14:18:20 +0200

    conanfile: try make work with cmake_find_package_multi generator

--- conanfile.py
+++ conanfile.py
@@ -1,40 +1,22 @@
 from conans import ConanFile, CMake, tools
-#from conan.tools.cmake import CMakeToolchain
+from conans.errors import ConanInvalidConfiguration
 import os
 
 class MdtCMakeModulesConan(ConanFile):
   name = "MdtCMakeModules"
-  #version = "0.6"
   license = "BSD 3-Clause"
   url = "https://gitlab.com/scandyna/mdt-cmake-modules"
   description = "Some CMake modules used in \"Multi Dev Tools\" projects"
-  #generators = "cmake", "cmake_paths", "CMakeDeps", "CMakeToolchain"
-  #generators = "cmake", "cmake_paths"
-  #generators = "CMakeToolchain"
   exports_sources = "Modules/*", "CMakeLists.txt", "MdtCMakeModulesConfig.cmake.in", "LICENSE"
 
   # TODO: maybe ue package_id() , sa generate()
 
-  # TODO should fail if no tag found ?
-  # Does conan provide a semver tool ??
   def set_version(self):
-    if os.path.exists(".git"):
+    if not self.version:
+      if not os.path.exists(".git"):
+        raise ConanInvalidConfiguration("could not get version from git tag.")
       git = tools.Git()
       self.version = "%s" % (git.get_tag())
-    #else:
-      #self.version = "None"
-
-  #def generate(self):
-    #tc = CMakeToolchain(self)
-    #tc.variables["MY_VAR"] = "/tmp"
-    #tc.variables.debug["MY_VAR"] = "/tmp"
-    #tc.variables.release["MY_VAR"] = "/tmp"
-    ##tc.variables.debug["CMAKE_PREFIX_PATH"] = "/tmp"
-    ##tc.blocks["find_paths"].values = {"":"/tmp"}
-    #tc.generate()
-    ##tc.find_paths = "/tmp"
-    ##tc.blocks["find_paths"].values = "/tmp"
-    ##tc.blocks["generic_system"].find_paths = self.package_folder
 
   def configure_cmake(self):
     cmake = CMake(self)
@@ -52,12 +34,7 @@ class MdtCMakeModulesConan(ConanFile):
     #cmake = CMake(self)
     #cmake.install()
 
-  #def package_id(self):
-    #self.info.header_only()
-
   def package_info(self):
-    #self.cpp_info.names["cmake_find_package_multi"] = ""
-
     # cmake_find_package_multi generator must be supported
     # if we want to use packages from conan-center
     # It will generate CMake targets that have no sense here,
@@ -65,8 +42,5 @@ class MdtCMakeModulesConan(ConanFile):
     # But, at least, tell conan to add the Modules
     # to CMAKE_MODULE_PATH (and also CMAKE_PREFIX_PATH)
     self.cpp_info.builddirs = ["Modules"]
-    #self.cpp_info.libs = ["Fake"]
-    #self.cpp_info.builddirs = ["cmake", "Modules"]
-    #self.cpp_info.build_modules["cmake_find_package_multi"].append("cmake")
 
 # See: https://bincrafters.readthedocs.io/en/latest/contributing_to_packages/package_guidelines_required.html
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,26 +1,10 @@
 from conans import ConanFile, CMake, tools
-#from conan.tools.cmake import CMakeToolchain
 import os
 
 class MdtCMakeModulesTestConan(ConanFile):
   #settings = "os"
   settings = "os", "compiler", "build_type", "arch"
-  #generators = "cmake_paths"
-  #generators = "CMakeDeps", "CMakeToolchain"
-  #generators = "CMakeToolchain"
   generators = "cmake_paths", "cmake_find_package_multi"
-
-  #def generate(self):
-    #tc = CMakeToolchain(self)
-    ##tc.variables["CMAKE_PREFIX_PATH"] = self.package_folder
-    ##tc.variables.debug["MY_VAR"] = "/tmp"
-    ##tc.variables.release["MY_VAR"] = "/tmp"
-    ##tc.variables.debug["CMAKE_PREFIX_PATH"] = "/tmp"
-    ##tc.blocks["find_paths"].values = {"":"/tmp"}
-    #tc.generate()
-    ##tc.find_paths = "/tmp"
-    ##tc.blocks["find_paths"].values = "/tmp"
-    ##tc.blocks["generic_system"].find_paths = self.package_folder
 
   def _build_with_cmake_paths(self):
     tools.mkdir("cmake_paths_folder")
@@ -40,10 +24,6 @@ class MdtCMakeModulesTestConan(ConanFile):
   def build(self):
     self._build_with_cmake_paths()
     self._build_with_cmake_find_package_multi()
-    #cmake = CMake(self)
-    #cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "%s/conan_toolchain.cmake" % (self.build_folder)
-    #cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "%s/conan_paths.cmake" % (self.build_folder)
-    #cmake.configure()
 
   def imports(self):
     self.copy("*.dll", dst="bin", src="bin")
