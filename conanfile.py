655df9aa7d2feb00f0545709763f2a042e1032de
Philippe Steinmann <philippe.steinmann@gmail.com>
2022-04-30 10:03:59 +0200

    Add support for new Conan CMake generators

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class MdtCMakeModulesConan(ConanFile):
   license = "BSD 3-Clause"
   url = "https://gitlab.com/scandyna/mdt-cmake-modules"
   description = "Some CMake modules used in \"Multi Dev Tools\" projects"
-  exports_sources = "Modules/*", "CMakeLists.txt", "MdtCMakeModulesConfig.cmake.in", "LICENSE"
+  exports_sources = "Modules/*", "CMakeLists.txt", "MdtCMakeModulesConfig.cmake.in", "LICENSE", "mdt_cmake_modules-conan-cmake-modules.cmake"
 
   # TODO: maybe ue package_id() , sa generate()
 
@@ -18,30 +18,33 @@ class MdtCMakeModulesConan(ConanFile):
       git = tools.Git()
       self.version = "%s" % (git.get_tag())
 
-  def configure_cmake(self):
+  def _configure_cmake(self):
     cmake = CMake(self)
     cmake.definitions["FROM_CONAN_PROJECT_VERSION"] = self.version
 
     return cmake
 
   def build(self):
-    cmake = self.configure_cmake()
+    cmake = self._configure_cmake()
     cmake.configure()
     cmake.build()
-    cmake.install()
-
-  #def package(self):
-    #cmake = CMake(self)
     #cmake.install()
 
+  def package(self):
+    cmake = self._configure_cmake()
+    cmake.install()
+    self.copy("mdt_cmake_modules-conan-cmake-modules.cmake")
+
   def package_info(self):
-    # cmake_find_package_multi generator must be supported
-    # if we want to use packages from conan-center
-    # It will generate CMake targets that have no sense here,
-    # I don't know how to avoid that
-    # But, at least, tell conan to add the Modules
-    # to CMAKE_MODULE_PATH (and also CMAKE_PREFIX_PATH)
-    #self.cpp_info.builddirs = ["Modules"]
-    self.cpp_info.builddirs = [".","Modules"]
+
+    self.cpp_info.includedirs = []
+    build_modules = ["mdt_cmake_modules-conan-cmake-modules.cmake"]
+
+    # This will be used by CMakeDeps
+    self.cpp_info.set_property("cmake_build_modules", build_modules)
+
+    # This must be added for other generators
+    self.cpp_info.build_modules["cmake_find_package"] = build_modules
+    self.cpp_info.build_modules["cmake_find_package_multi"] = build_modules
 
 # See: https://bincrafters.readthedocs.io/en/latest/contributing_to_packages/package_guidelines_required.html
---
+++ mdt_cmake_modules-conan-cmake-modules.cmake
@@ -0,0 +1,27 @@
+
+message(DEBUG "begin of mdt_cmake_modules-conan-cmake-modules.cmake")
+
+message(DEBUG "CMAKE_CURRENT_LIST_DIR: ${CMAKE_CURRENT_LIST_DIR}")
+
+message(DEBUG "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
+
+# TODO should implement this
+# mdt_generate_mdt_find_path_in_list_function()
+# mdt_write_mdt_find_path_in_list_function()
+# mdt_find_path_in_list()
+
+# We could have a trailing slash, or not, in CMAKE_PREFIX_PATH
+list(REMOVE_ITEM CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/")
+list(REMOVE_ITEM CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}")
+# On Windows build, we could be a trailing slash or backslash
+if(WIN32)
+  list(REMOVE_ITEM CMAKE_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}\\")
+endif()
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/Modules")
+
+message(DEBUG "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
+
+# unset(MDT_CMAKE_CURRENT_LIST_DIR_INDEX)
+
+message(DEBUG "end of mdt_cmake_modules-conan-cmake-modules.cmake")
